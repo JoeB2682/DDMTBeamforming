fc5702f21a32ceb122b7ce8bf5c27691cafdf565
JoeB2682 <mhn547@york.ac.uk>
2026-06-18 09:01:48 +0100

    Organised MATLAB dir and starting on array tests

---
+++ MATLAB/ArrayTesting/MainScript.m
@@ -0,0 +1,18 @@
+%==========================================================================
+%% Beamforming Main Script
+%
+% MSc Audio and Music Technology: Research Project
+%
+% Tests beamforming algorithms on the circular array in T/222
+%
+% Created By: Joseph Adam Bozzo
+% mhn547
+% 303013659
+%==========================================================================
+%% System
+clear all;
+clc;
+
+Fs = 44100;
+t = (0:Fs-1)/Fs; 
+%==========================================================================
